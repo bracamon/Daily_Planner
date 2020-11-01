f08c1a87bb52ca187d3bce1decf324dbc4890f4e
bracamon <bracamontes.ochoa@gmail.com>
2020-10-31 17:16:28 -0700

    established key and value for local storage based on each hour

--- assets/app.js
+++ assets/app.js
@@ -1,9 +1,20 @@
-var currentTime = moment().format('MMMM Do YYYY, h:mm a')
-console.log(currentTime)
+$(document).ready(function () {
+    var currentTime = moment().format('MMMM Do YYYY, h:mm a');
+    $("#currentDay").text(currentTime);
 
-$("#currentDay").text(currentTime);
+    var times = [9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19];
 
-$("form").on("submit", function(event) {
-    event.preventDefault()
-    console.log(event.target);
-});
+    times.forEach(time => {
+        var timeCheck = window.localStorage.getItem(time);
+        console.log(timeCheck);
+        if(timeCheck === null) {
+            window.localStorage.setItem(time, "");
+        }
+    });
+
+    $("form").on("submit", function(event) {
+        event.preventDefault()
+        console.log(event.target.querySelector("input").getAttribute("id"));
+        console.log(event.target.querySelector("input").value)
+    });
+})

57b9b440dc2b433214932d7beac4e4b0ebb20592
bracamon <bracamontes.ochoa@gmail.com>
2020-10-31 17:37:33 -0700

    added if/else if/else conditionals comparing current time vs time in planner to set background color and text color for each line

--- assets/app.js
+++ assets/app.js
@@ -8,12 +8,22 @@ $(document).ready(function () {
         var timeCheck = window.localStorage.getItem(time);
 
         var currentHour = moment().hour()
-        console.log(currentHour)
+        // console.log(currentHour)
+        console.log(time)
+
+        if(currentHour > time){
+            $(`#${time}`).addClass("bg-danger text-light");
+            $(`#${time}`).attr("disabled", true);
+        } else if(currentHour === time) {
+            $(`#${time}`).addClass("bg-secondary text-light");
+        }
 
         if(timeCheck === null) {
             window.localStorage.setItem(time, "");
         } else if(timeCheck.length > 0){
             $(`#${time}`).attr("value", window.localStorage.getItem(time))
+        } else {
+            $(`#${time}`).addClass("bg-success text-light");
         }
     });
 
